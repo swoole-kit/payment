95e3a3dbcf49b234dc294a081cfafad756cb85e1
evalor <mipone@foxmail.com>
2019-09-27 14:07:46 +0800

    支付类库

---
+++ src/Configs/WeChat.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SwooleKit\Payment\Configs;
+
+use EasySwoole\Spl\SplBean;
+
+/**
+ * 微信支付配置
+ * Class WeChat
+ * @package SwooleKit\Payment\Configs
+ */
+class WeChat extends SplBean
+{
+
+}
---
+++ src/Payment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SwooleKit\Payment;
+
+/**
+ * 统一操作入口
+ * Class Payment
+ * @package SwooleKit\Payment
+ */
+class Payment
+{
+
+}
